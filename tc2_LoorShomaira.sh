d389706ce434844c0670ea2bdacfa313f336f608
loorshomairabiotech <117690592+loorshomairabiotech@users.noreply.github.com>
2022-11-09 13:27:57 -0500

    se ha subodo el Tc2
    Shomaira Loor ha realizado una modificación

---
+++ tc2_LoorShomaira.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo -e "\n"
+echo "Comando para escoger las primeras  7 filas"
+echo -e "\n"
+head -n 7 EigenBetw.tsv
+echo -e "\n"
+echo "Comando para esocger las ultimas 5 filas"
+tail -n 5 EigenBetw.tsv
